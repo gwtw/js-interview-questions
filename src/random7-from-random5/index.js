96726b32875d462381bd5c6aa6c3c8f00dbb02d3
Daniel Imms <tyriar@tyriar.com>
2015-06-21 14:31:26 -0700

    Fix comment

--- src/random7-from-random5/index.js
+++ src/random7-from-random5/index.js
@@ -14,7 +14,7 @@
  * @returns {number} A number from 1 to 7.
  */
 function random7(random5) {
-  // Get 0, 5, 10, 15 or 20 then add 0-4 (4% chance for 1-25)
+  // Get 0, 5, 10, 15 or 20 then add 0-4 (4% chance for 0-24)
   var val = (random5() - 1) * 5 + (random5() - 1);
   // If 0-20, return the result modulo 7 + 1 (12% chance for 1-7), otherwise
   // call recursively (16% chance)
