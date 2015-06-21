d7f6ce19e1fd1a72d6d4fcc6df2aa5dae0b70f0d
Daniel Imms <tyriar@tyriar.com>
2015-06-21 14:29:39 -0700

    Change random7 result order

--- src/random7-from-random5/index.js
+++ src/random7-from-random5/index.js
@@ -14,11 +14,11 @@
  * @returns {number} A number from 1 to 7.
  */
 function random7(random5) {
-  // Get 0, 5, 10, 15 or 20 then add 1-5 (4% chance for 1-25)
-  var val = (random5() - 1) * 5 + random5();
-  // If 1-21, return the result modulo 7 (12% chance for 1-7), otherwise call
-  // recursively (16% chance)
-  return val > 21 ? random7(random5) : val % 7 + 1;
+  // Get 0, 5, 10, 15 or 20 then add 0-4 (4% chance for 1-25)
+  var val = (random5() - 1) * 5 + (random5() - 1);
+  // If 0-20, return the result modulo 7 + 1 (12% chance for 1-7), otherwise
+  // call recursively (16% chance)
+  return val >= 21 ? random7(random5) : val % 7 + 1;
 }
 
 module.exports = random7;
